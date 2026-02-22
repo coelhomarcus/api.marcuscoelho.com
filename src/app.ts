04e9987afe3abad8d54c8fcda7b8cd6107efd43a
Marcus Coelho <qaizencoelho@gmail.com>
2026-02-22 15:15:19 -0300

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import { contactRouter } from "./modules/contact/contact.routes";
 
 const app = express();
 
+app.use(cors({ origin: ["https://marcuscoelho.com"] }));
 app.use(express.json());
 
 app.use("/contact", contactRouter);
